e981c2dac9e475688c07586d0a5d0e514b450f90
henry <yuanrui.gao@my.jcu.edu.au>
2025-02-16 00:13:04 +0800

    second push（Initial operation procedures）

--- app/src/main/java/com/example/as1_yuanrui_gao/MainActivity.kt
+++ app/src/main/java/com/example/as1_yuanrui_gao/MainActivity.kt
@@ -4,7 +4,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -12,9 +15,11 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import com.example.as1_yuanrui_gao.ui.theme.As1_Yuanrui_GaoTheme
 
@@ -24,20 +29,30 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             As1_Yuanrui_GaoTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Henry",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                    ColumnList()
-                }
+                Scaffold(
+                    topBar = {
+                        TopAppBarDemo("Henry")
+                    },
+                    content = { paddingValues ->
+                        Column(
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .padding(paddingValues)
+                                .background(Color.Gray.copy(alpha = 0.1f)),
+                            verticalArrangement = Arrangement.Center,
+                            horizontalAlignment = Alignment.CenterHorizontally
+                        ) {
+                            ColumnList()
+                        }
+                    }
+                )
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun TopAppBarDemo(name: String) {
     Text(
         modifier = Modifier
             .fillMaxWidth()
@@ -46,7 +61,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     )
 }
 
-@Preview(showBackground = true)
 @Composable
 fun ColumnList() {
     LazyColumn(
@@ -54,19 +68,21 @@ fun ColumnList() {
             .fillMaxSize()
             .background(Color.LightGray)
     ) {
-        items(1){
-            Greeting(
-                name = "Henry"
+        item {
+            Image(
+                painter = painterResource(id = R.drawable.page1),
+                contentDescription = "Page 1 Image",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(10.dp),
+                contentScale = ContentScale.Crop
             )
         }
-        imageView.setImageResource(R.drawable.page1)
-        items(1) {
+        items(2) {
             Text(
-                modifier = Modifier
-                   .padding(0.dp),
+                modifier = Modifier.padding(40.dp),
                 text = "Item number $it",
             )
-
         }
     }
 }
